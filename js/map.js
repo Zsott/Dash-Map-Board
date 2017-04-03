61926d69bdd39eb3efafb3660fd4afa6b2b496fd
Zsolt Ónodi <Zsott@users.noreply.github.com>
2017-04-03 12:21:53 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -129,7 +129,7 @@ require(
 
 			//Az adatréteget beilleszti a rétegek tömb-be a config-ban megadott index értékkel
 			var layerUrl = actTheme["additionalLayerURLs"];
-			layerUrl.splice(actTheme["dataServiceIndex"], 0, actTheme["dataServiceURL"]);
+			layerUrl.splice(actTheme.dataServiceLayerPosition, 0, actTheme.dataServiceURL);
 			
 			//Egyéb rétegek hozzáadása a térképhez.
 			layerUrl.forEach(function(entry) {
@@ -152,7 +152,7 @@ require(
 			jarasQuery = new esri.tasks.Query();
 			jarasQuery.returnGeometry = true;
 			jarasQuery.outFields = ["*"];
-			jarasQueryTask = new QueryTask(actTheme["dataServiceURL"] + "/0");
+			jarasQueryTask = new QueryTask(actTheme.dataServiceURL + "/" + actTheme.dataServiceLayerIndex);
 
 			//TimeSlider inicializálása
 			initTimeSlider();
@@ -302,13 +302,13 @@ require(
                     $("#leftPanel").append('<div id="' + actTheme.chartPositions.LL + '" class="roundedBox panels"></div>');
                     $("#" + actTheme.chartPositions.LL).css({
                         "height" : "calc(100% - 2px)"
-                    });		
+                    });
                 }
                 else if(actTheme.layout.mapHeightPercent == 100){
                     // A térkép átméretezése
                     $("#map").css({
                         "height" : "calc(100% - 2px)"
-                    });		
+                    });
                 }
                 else if(actTheme.layout.mapHeightPercent > 0 && actTheme.layout.mapHeightPercent < 100){
                     // A térkép magasságának átméretezése
@@ -316,13 +316,14 @@ require(
                         "height" : "calc(" + actTheme.layout.mapHeightPercent + "% - 6px)",
                         "margin-bottom" : "4px"
                     });
+                    $("#map").append('<img src="img/enlarge.png" class="enlargeImg"/>');
                     // A bal alsó diagram panel létrehozása, méretezése
                     $("#leftPanel").append('<div id="' + actTheme.chartPositions.LL + '" class="roundedBox panels"></div>');
                     var h = 100 - actTheme.layout.mapHeightPercent;
                     $("#" + actTheme.chartPositions.LL).css({
                         "height" : "calc(" + h + "% - 6px)",
                         "margin-top" : "4px"
-                    });		
+                    });
                 }
                 else{
                     // ##### Kiírja a hibaüzenetet, de aztán megcsinálja a function többi részét. Jó ez így?
@@ -387,7 +388,7 @@ require(
                     outFields=["*"];
                     neighQuery.outFields = outFields;
                     neighQuery.spatialRelationship = esri.tasks.Query.SPATIAL_REL_TOUCHES;
-                    var selNeighbour = new QueryTask(actTheme["dataServiceURL"] + "/0"); 
+                    var selNeighbour = new QueryTask(actTheme.dataServiceURL + "/" + actTheme.dataServiceLayerIndex); 
                     selNeighbour.execute(neighQuery, createBubDP);                    
 				}
 				else if(actTheme["chartPositions"][keyTitle]=="rad"){
@@ -448,8 +449,7 @@ require(
 						}
 					}
                 }
-				serDP.push(json);
-				
+				serDP.push(json);				
 			});
 			createSerialChart(serDP);
 		}
@@ -533,7 +533,7 @@ require(
 				timeSlider = new TimeSlider({},"timeSlider");
 				firstTheme=false;
 			}
-
+            terkep.setTimeSlider(timeSlider);
             timeSlider.on("time-extent-change",extentChanged);
             
             timeSlider.setThumbCount(1);
@@ -604,7 +604,7 @@ require(
                     "markerSize": 14,
                     "valueText": ""
                 }
-            });            
+            });
         }
 		
 		//Serial chart elkészítése
@@ -651,8 +651,7 @@ require(
                     "axisAlpha": 0,
                     "position": "left",
                     "title": "fő",
-                    "stackType": "regular",
-                    //"totalText": "[[total]]"
+                    "stackType": "regular"
                 }],
                 "startDuration": 1,
                 "graphs": graphs,
@@ -676,41 +675,48 @@ require(
                 },
                 "export": {"enabled": true}
             });
-			
         }
 
-		//Tálázat elkészítése
+		//Create HTML table in #tab div
 		function createTable(dp){
-			console.log("NAIDE");
-			console.log(dp);
-			var htmlString = '<table>';
+            $("#tab").append('<div id="tableTitle"></div>');
+            $("#tab").append('<div id="tableContent"></div>');
+            var htmlString = '<table>';
 			htmlString = htmlString + '<tr class="tableHeading"><td>' + actTheme.tableSettings.heading + '</td>';
 			for(var ev in actTheme.timeStops){
 				htmlString = htmlString + '<td>' + actTheme.timeStops[ev] + '</td>';
 			}
 			htmlString = htmlString + '</tr>';
-			for (var keyTitle in actTheme["tableSettings"].fieldMap) {
-				htmlString = htmlString + '<tr>';
+			for (var keyTitle in actTheme["tableSettings"].fieldMap){
+				htmlString = htmlString + '<tr><td class="tableRowId">' + actTheme.tableSettings.fieldMap[keyTitle] + '</td>';
 				for (var i = 0; i < dp.length; i++) {
-					htmlString = htmlString + '<td>' + dp[i][keyTitle] + '</td>';
-					if(i == dp.length - 1){
+                    // checkRounding(dp[i][keyTitle],actTheme.tableSettings.dataPrecision);
+					// htmlString = htmlString + '<td>' + dp[i][keyTitle] + '</td>';
+					var cValue = checkRounding(dp[i][keyTitle],actTheme.tableSettings.dataPrecision);
+                    htmlString = htmlString + '<td>' + cValue + '</td>';
+                    if(i == dp.length - 1){
 						htmlString = htmlString + '</tr>';
 					}
 				}
 			}
 			htmlString = htmlString + '</table>';
-            $("#tab").html(htmlString);
-            $("#tab table").css({
-                "font-size" : "10px",
-                "margin" : "0 auto",
-                "border-collapse" : "collapse"
-            });
-            $("#tab table tr, #tab table td").css({
-                "border" : "1px solid black",
-                "padding" : "1px"
-            });
+            $("#tableContent").html(htmlString);
+            $("#tableTitle").html(actTheme.tableSettings.title);
 		}
 
+        //Rounding function for table creation
+        function checkRounding(value,prec){
+            var rValue = value.toFixed(prec).split(".");
+            if(rValue[0] == 0 && rValue[1] == 0){
+                return 0;
+            }
+            else if(rValue[1] == 0){
+                return value;
+            }
+            else{
+                return value.toFixed(prec).replace(".",",");
+            }
+        }
 		
 		//Bubble chart elkészítése
         function createBubbleChart(dp){
